31f3139b5eff273592c9e39838f0cf0aec08c9fb
noViceMin <admin@isamin.kr>
2025-01-15 20:56:50 +0900

    feat: news basic

--- app/routes/_navbar.news/route.tsx
+++ app/routes/_navbar.news/route.tsx
@@ -1,3 +1,23 @@
+import "~/styles/news.css";
+
 export default function News() {
-  return <p>News</p>;
+  return (
+    <div id="pages">
+      <div id="page-container" className="">
+        <div id="page" className="news">
+          <div className="header">
+            <h3 className="title-m">News</h3>
+            <div className="category">
+              <div className="item selected">
+                <p>News</p>
+              </div>
+              <div className="item">
+                <p>Portfolio</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
